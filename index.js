864bbf9a9e5e634ebc068b4524d9449147ae86a6
OJPARKINSON <olliepar01@gmail.com>
2020-05-27 20:37:24 +0100

    not deleted import

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const cors = require('cors');
-const morgan = require('morgan');
 const { ApolloServer } = require('apollo-server-express');
 
 const { typeDefs } = require('./src/models/schema');
